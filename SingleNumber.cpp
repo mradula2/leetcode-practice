4b793fd1f5f05dda792534b26867f91577a0fbd6
Mradula <mradula.nayak@nordstrom.com>
2025-03-25 14:29:29 -0700

    finding a single number without a duplicate

---
+++ SingleNumber.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+};
